b36d0b689250b642832221b795d709f200236565
stephane de la torre <s.dlt@hotmail.fr>
2018-07-31 17:57:36 +0200

    session 3/16

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-import App from "./App";
+import App from "../App";
 
 it("shows a comment box", () => {
   const div = document.createElement("div");
   ReactDom.render(<App />, div);
+  expect(div.innerHTML).toContain("Comment Box");
   ReactDom.unmountComponentAtNode(div);
 });
